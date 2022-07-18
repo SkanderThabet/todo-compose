a22d67d053e61cc55c10945f29dff7285f3bd30b
SkanderThabet <skander.thabet@esprit.tn>
2022-07-18 18:03:18 +0100

    Setup Dagger-hilt, created ToDo app with DI, package DI and declared Room DB singleton

--- app/src/main/java/com/example/todo_compose/MainActivity.kt
+++ app/src/main/java/com/example/todo_compose/MainActivity.kt
@@ -3,15 +3,10 @@ package com.example.todo_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.todo_compose.ui.theme.TodoComposeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
