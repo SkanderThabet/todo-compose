c90cb8aa432a48b15a24fe3caabc3a0ede1aed7e
SkanderThabet <skander.thabet@esprit.tn>
2022-07-18 03:01:30 +0100

    Created util folder for global constants, structured the model for the Todo Task

--- app/src/main/java/com/example/todo_compose/data/models/Priority.kt
+++ app/src/main/java/com/example/todo_compose/data/models/Priority.kt
@@ -1,10 +1,15 @@
 package com.example.todo_compose.data.models
 
-import android.graphics.Color
+import androidx.compose.ui.graphics.Color
+import com.example.todo_compose.ui.theme.HighPriorityColor
+import com.example.todo_compose.ui.theme.LowPriorityColor
+import com.example.todo_compose.ui.theme.MediumPriorityColor
+import com.example.todo_compose.ui.theme.NonePriorityColor
+
 
 enum class Priority(val color: Color) {
-    HIGHT(),
-    MEDUIM(),
-    LOW(),
-    NONE()
+    HIGHT(HighPriorityColor),
+    MEDUIM(MediumPriorityColor),
+    LOW(LowPriorityColor),
+    NONE(NonePriorityColor)
 }
--- app/src/main/java/com/example/todo_compose/data/models/ToDoTask.kt
+++ app/src/main/java/com/example/todo_compose/data/models/ToDoTask.kt
@@ -1,8 +1,15 @@
 package com.example.todo_compose.data.models
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.todo_compose.util.Constants.DATABASE_TABLE
 
-@Entity
-data class ToDoTask() {
+@Entity(tableName = DATABASE_TABLE)
+data class ToDoTask(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int= 0,
+    val title:String,
+    val description:String,
+    val priority: Priority
 
-}
+)
