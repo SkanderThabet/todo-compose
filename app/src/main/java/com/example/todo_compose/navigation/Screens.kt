9601b24dd9dd24d2bc1c7fca9fb2957a25c5731a
SkanderThabet <skander.thabet@esprit.tn>
2022-07-19 17:29:12 +0100

    Configured Screens for List's task and actions

--- app/src/main/java/com/example/todo_compose/navigation/Screens.kt
+++ app/src/main/java/com/example/todo_compose/navigation/Screens.kt
@@ -1,4 +1,19 @@
 package com.example.todo_compose.navigation
 
-class Screens {
+import androidx.navigation.NavHostController
+import com.example.todo_compose.util.Action
+import com.example.todo_compose.util.Constants.LIST_SCREEN
+
+class Screens(navHostController: NavHostController) {
+    val list: (Action) -> Unit =
+        {
+            navHostController.navigate("list/${it.name}") {
+                popUpTo(LIST_SCREEN) {
+                    inclusive = true
+                }
+            }
+        }
+    val task: (Int) -> Unit = {
+        navHostController.navigate("task/$it")
+    }
 }
--- app/src/main/java/com/example/todo_compose/util/Constants.kt
+++ app/src/main/java/com/example/todo_compose/util/Constants.kt
@@ -1,6 +1,8 @@
 package com.example.todo_compose.util
 
-object Constants  {
+object Constants {
     const val DATABASE_TABLE = "todo_table"
     const val DATABASE_NAME = "todo_database"
+    const val TASK_SCREEN = "task/{taskId}"
+    const val LIST_SCREEN = "list/{action}"
 }
