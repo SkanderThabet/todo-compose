a2c07eb991d922d6d6823bda95fa414a4ce9d913
SkanderThabet <skander.thabet@esprit.tn>
2022-07-19 16:55:33 +0100

    implemented fetching all tasks via couroutines

--- app/src/main/java/com/example/todo_compose/data/repositories/ToDoRepository.kt
+++ app/src/main/java/com/example/todo_compose/data/repositories/ToDoRepository.kt
@@ -7,7 +7,7 @@ import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 @ViewModelScoped
-class ToDoRepositories @Inject constructor(private val toDoDao: ToDoDao) {
+class ToDoRepository @Inject constructor(private val toDoDao: ToDoDao) {
 
     val getAllTasks: Flow<List<ToDoTask>> = toDoDao.getAllTasks()
     val sortByLowPriority: Flow<List<ToDoTask>> = toDoDao.sortByLowPriority()
