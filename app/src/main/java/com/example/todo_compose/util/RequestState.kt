afccfafe77efb70e991cc7566f0c15026bb83417
SkanderThabet <skander.thabet@esprit.tn>
2022-07-23 18:10:03 +0100

    Made a sealed Class for different states

--- app/src/main/java/com/example/todo_compose/util/RequestState.kt
+++ app/src/main/java/com/example/todo_compose/util/RequestState.kt
@@ -1,3 +1,9 @@
 package com.example.todo_compose.util
 
-sealed class RequestState
+sealed class RequestState<out T> {
+    object Idle : RequestState<Nothing>()
+    object Loading : RequestState<Nothing>()
+    data class Success<T>(val data: T) : RequestState<T>()
+    data class Error(val error: Throwable) : RequestState<Nothing>()
+
+}
