2c6645cf32de88df255e7f42501b4972c07f3407
SkanderThabet <skander.thabet@esprit.tn>
2022-07-25 18:33:24 +0100

    Fixed width sized of Priority drop menu items, next i'll implement the logic to add tasks

--- app/src/main/java/com/example/todo_compose/ui/screens/task/TaskScreen.kt
+++ app/src/main/java/com/example/todo_compose/ui/screens/task/TaskScreen.kt
@@ -2,6 +2,7 @@ package com.example.todo_compose.ui.screens.task
 
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import com.example.todo_compose.data.models.Priority
 import com.example.todo_compose.data.models.ToDoTask
 import com.example.todo_compose.util.Action
 
@@ -17,6 +18,15 @@ fun TaskScreen(
                 selectedTask = selectedTask
             )
         },
-        content = {}
+        content = {
+            TaskContent(
+                title = "",
+                onTitleChange = {},
+                description = "",
+                onDescriptionChange = {},
+                priority = Priority.HIGH,
+                onPrioritySelected = {}
+            )
+        }
     )
 }
